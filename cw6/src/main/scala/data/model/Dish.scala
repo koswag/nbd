fb34172df96b18692e07f77406163d8b109a282d
kskarzynski <k.skarzynski@sages.pl>
2022-06-16 12:49:02 +0200

    Add JSON serialization format definitions

--- cw6/src/main/scala/data/model/Dish.scala
+++ cw6/src/main/scala/data/model/Dish.scala
@@ -1,9 +1,17 @@
 package pl.edu.pja.s15165.cw6
 package data.model
 
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
+
+
 case class Dish(
     name: String,
     description: String,
     price: Int,
     ingredients: List[Ingredient]
 )
+
+object Dish {
+    implicit val jsonFormat: RootJsonFormat[Dish] = jsonFormat4(Dish.apply)
+}
--- cw6/src/main/scala/data/model/Ingredient.scala
+++ cw6/src/main/scala/data/model/Ingredient.scala
@@ -1,7 +1,15 @@
 package pl.edu.pja.s15165.cw6
 package data.model
 
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
+
+
 case class Ingredient(
     name: String,
     quantity: Int
 )
+
+object Ingredient {
+    implicit val jsonFormat: RootJsonFormat[Ingredient] = jsonFormat2(Ingredient.apply)
+}
