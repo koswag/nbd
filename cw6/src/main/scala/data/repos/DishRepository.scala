a4506345eff9e1dd34747357d495e4af4e1e8c27
kskarzynski <k.skarzynski@sages.pl>
2022-06-16 13:14:30 +0200

    Dish repository definition

---
+++ cw6/src/main/scala/data/repos/DishRepository.scala
@@ -0,0 +1,31 @@
+package pl.edu.pja.s15165.cw6
+package data.repos
+
+import data.db.Riak
+
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import data.model.Dish
+
+
+class DishRepository extends Actor with ActorLogging {
+    import data.proto.DishRepositoryProtocol._
+    import context.dispatcher
+
+    private val dishes = Riak.client.bucket("dishes")
+
+    override def receive: Receive = {
+        case StoreDish(dish) => storeDish(dish, sender)
+        case FetchDishByName(name) => fetchDishByName(name, sender)
+    }
+
+    private def storeDish(dish: Dish, actor: ActorRef): Unit =
+        dishes.storeAndFetch(dish.name, dish)
+            .map(_.as[Dish])
+            .onSuccess { case storedDish => actor ! storedDish }
+
+    private def fetchDishByName(name: String, actor: ActorRef): Unit =
+        dishes.fetch(name)
+            .map { rawOption => rawOption.map(_.as[Dish]) }
+            .onSuccess { case dishOption => actor ! dishOption }
+
+}
